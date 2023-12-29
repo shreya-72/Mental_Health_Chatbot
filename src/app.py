6887078bc5522b3725d7a9a5aa4c8a70f80c72b0
shreya-72 <shreyarnaik03@gmail.com>
2023-12-29 19:02:06 +0530

    first commit

---
+++ src/app.py
@@ -0,0 +1,93 @@
+from flask import Flask, request, jsonify
+
+
+from helper.openai_api import text_complition
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'All is well...'
+
+
+@app.route('/dialogflow/es/receiveMessage', methods=['POST'])
+def esReceiveMessage():
+    try:
+        data = request.get_json()
+        # You can use this action to do different things
+        # action = data['queryResult']['action']
+        query_text = data['queryResult']['queryText']
+
+        result = text_complition(query_text)
+
+        # if result['status'] == 1:
+        #     return jsonify(
+        #         {
+        #             'fulfillmentText': result['response']
+        #         }
+        #     )
+        if result['status'] == 1:
+            return jsonify({
+                'fulfillmentText': result['response']
+            })
+        else:
+            return jsonify({
+                'fulfillmentText': 'OpenAI API call failed.'
+            })
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+    return jsonify(
+        {
+            'fulfillmentText': f'Something went wrong: {str(e)}'
+        }
+    )
+
+
+# @app.route('/dialogflow/cx/receiveMessage', methods=['POST'])
+# def cxReceiveMessage():
+#     try:
+#         data = request.get_json()
+#         # Use this tag peoperty to choose the action
+#         # tag = data['fulfillmentInfo']['tag']
+#         query_text = data['text']
+
+#         result = text_complition(query_text)
+
+#         if result['status'] == 1:
+#             return jsonify(
+#                 {
+#                     'fulfillment_response': {
+#                         'messages': [
+#                             {
+#                                 'text': {
+#                                     'text': [result['response']],
+#                                     'redactedText': [result['response']]
+#                                 },
+#                                 'responseType': 'HANDLER_PROMPT',
+#                                 'source': 'VIRTUAL_AGENT'
+#                             }
+#                         ]
+#                     }
+#                 }
+#             )
+#     except:
+#         pass
+#     return jsonify(
+#         {
+#             'fulfillment_response': {
+#                 'messages': [
+#                     {
+#                         'text': {
+#                             'text': ['Something went wrong.'],
+#                             'redactedText': ['Something went wrong.']
+#                         },
+#                         'responseType': 'HANDLER_PROMPT',
+#                         'source': 'VIRTUAL_AGENT'
+#                     }
+#                 ]
+#             }
+#         }
+#     )
